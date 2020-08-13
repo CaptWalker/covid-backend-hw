fb543aa4616499e9ce0b9a8e60bf2ad82122b2cd
CaptWalker <dhruvasharma151996@gmail.com>
2020-08-13 19:03:42 +0530

    update hello controllers

--- src/main/java/com/ibm/hello/controller/HelloController.java
+++ src/main/java/com/ibm/hello/controller/HelloController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
